93fb4bae99fa6dcd796af35ba3d4425853b0d72b
Son Vu <40129621+sonvule59@users.noreply.github.com>
2019-07-12 14:34:38 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 alert(4);
 alert(5);
-var fromStorage = localStorage.getItem('test')
+var fromStorage = localStorage.getItem('test');
 alert("This was retrieved from script.js "+JSON.stringify(fromStorage));
 console.log(fromStorage);
